35dff58a074573ddb885eb22d6ddf3b20ccc2ecf
david <davidcodingai@gmail.com>
2025-12-15 21:04:23 +0800

    Initial Commit

---
+++ FinanceTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinanceTracker
+//
+//  Created by david wu on 12/15/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
